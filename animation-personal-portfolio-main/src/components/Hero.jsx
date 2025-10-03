cecbc6c0c8c9f5c3c0037ecd5392969fb90e014e
CHIRAG-CHOKEN <chokenchirag89@gmail.com>
2025-10-04 01:43:11 +0530

    new change

--- animation-personal-portfolio-main/src/components/Hero.jsx
+++ animation-personal-portfolio-main/src/components/Hero.jsx
@@ -40,7 +40,7 @@ export const Hero = () => {
             variants={fadeInUp}
             whileHover={{ scale: 1.02 }}
           >
-            Chirag choken
+            Kritee Shukla
           </motion.h1>
           <motion.h2 className="hero-subtitle" variants={fadeInUp}>
             Creative Developer & Designer
